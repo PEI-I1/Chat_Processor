bb2a02589b75b69dddd77f39f62a44f018822994
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-11-04 15:19:31 +0000

    adicionadas palavras as categorias

--- categoria_dic.py
+++ categoria_dic.py
@@ -1,17 +1,18 @@
 cat = {
-    'linhas_apoio' : ['linhas', 'apoio','assunto'],
-    'phone_model' : ['modelos', 'telemóveis','pontos'],
-    'brand_phones' : ['comprar','telemóvel','preço'],
-    'top_phones' : ['top','telemóveis','melhores'],
-    'promo_phones' : ['promoções','telemóveis'],
-    'new_phones' : ['novidades','novos','telemóveis'],
-    'ofer_phones' : ['oferta','telemóveis'],
-    'prest_phones' : ['prestações','prestação','telemóveis'],
-    'points_phones' : ['pontos','telemóveis'],
-    'phones_by_price' : ['preço','telemóveis'],
-    'wtf_name' : '',
-    'stores_by_zone' : ['lojas'],
-    'store_address' : '',
+    'linhas_apoio' : ['linhas','linha','apoio','assunto','número','contacto','contactos','empresa','empresas','aderir','informações','adicionais','informação','adicional','serviços','chamada','chamadas','ligar'],
+    'phone_model' : ['modelos','modelo','telemóvel','telemóveis'],
+    'brand_phones' : ['marca','marcas','comprar','telemóvel','telemóveis','preço'],
+    'top_phones' : ['top','telemóvel','telemóveis','melhores','link'],
+    'promo_phones' : ['promoções','promoção','desconto','descontos','barato','baratos','telemóvel','telemóveis'],
+    'new_phones' : ['novidades','novos','telemóvel','telemóveis','recente','recentes'],
+    'ofer_phones' : ['oferta','telemóvel','telemóveis'],
+    'prest_phones' : ['prestações','prestação','telemóvel','telemóveis'],
+    'points_phones' : ['ponto','pontos','telemóvel','telemóveis'],
+    'phones_by_price' : ['ordenar','preço','telemóvel','telemóveis'],
+    'all_wtf' : ['tarifários','telemóvel','wtf']
+    'wtf_name' : ['tarifário','wtf'],
+    'stores_by_zone' : ['loja','lojas','zona'],
+    'store_address' : ['morada','loja','lojas'],
 }
 
 categoria = {
