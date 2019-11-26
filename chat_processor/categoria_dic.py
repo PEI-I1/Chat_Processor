90ea4a01dfc5fdd60b81051c060d62aa6f926ccf
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-11-26 10:06:50 +0000

    Adicionado campos no categoria_dic

--- chat_processor/categoria_dic.py
+++ chat_processor/categoria_dic.py
@@ -4,125 +4,161 @@ cat = [
         'service': 'FS_SCRAPER',
         'words': ['linhas','linha','apoio','assunto','número','contacto','contactos','empresa','empresas','aderir','informações','adicionais','informação','adicional','serviços','chamada','chamadas','ligar','falar',"assistente"],
         'params': ['SUBJECT'],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'phone_model',
         'service': 'FS_SCRAPER',
         'words': ['modelos','modelo','telemóvel','telemóveis'],
         'params': ['PRODUCT'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'brand_phones',
         'service': 'FS_SCRAPER',
         'words': ['marca','marcas','comprar','telemóvel','telemóveis','preço'],
         'params': ['ORG'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'top_phones',
         'service': 'FS_SCRAPER',
         'words': ['top','telemóvel','telemóveis','melhores','link'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'promo_phones',
         'service': 'FS_SCRAPER',
         'words': ['promoções','promoção','desconto','descontos','barato','baratos','telemóvel','telemóveis'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'new_phones',
         'service': 'FS_SCRAPER',
         'words': ['novidades','novos','telemóvel','telemóveis','recente','recentes'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'ofer_phones',
         'service': 'FS_SCRAPER',
         'words': ['oferta','telemóvel','telemóveis'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'prest_phones',
         'service': 'FS_SCRAPER',
         'words': ['prestações','prestação','telemóvel','telemóveis'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'points_phones',
         'service': 'FS_SCRAPER',
         'words': ['ponto','pontos','telemóvel','telemóveis'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'phones_by_price',
         'service': 'FS_SCRAPER',
         'words': ['ordenar','preço','telemóvel','telemóveis'],
         'params': ['MONEY', 'MONEY'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'all_wtf',
         'service': 'FS_SCRAPER',
         'words': ['tarifários','telemóvel','wtf'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'stores_by_zone',
         'service': 'FS_SCRAPER',
         'words': ['loja','lojas','zona'],
         'params': ['GPE'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'store_address',
         'service': 'FS_SCRAPER',
         'words': ['morada','loja','lojas'],
         'params': ['FAC'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'packages',
         'service': 'FS_SCRAPER',
         'words': ['pacotes'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'fiber_packages',
         'service': 'FS_SCRAPER',
         'words': ['pacotes', 'fibra'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'satelite_packages',
         'service': 'FS_SCRAPER',
         'words': ['pacotes', 'satelite', 'satélite'],
         'params': [],
-        'canRequestWithoutParams': True
+        'paramsOpcional': [],
+        'canRequestWithoutParams': True,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'packages_service',
         'service': 'FS_SCRAPER',
         'words': ['ordenar','preço','pacote','pacotes'],
         'params': ['PACKAGE'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     },
     {
         'request': 'packages_price',
         'service': 'FS_SCRAPER',
         'words': ['ordenar','preço','pacote','pacotes'],
         'params': ['MONEY', 'MONEY'],
-        'canRequestWithoutParams': False
+        'paramsOpcional': [],
+        'canRequestWithoutParams': False,
+        'needAtLeastOneParam': False
     }
 ]
