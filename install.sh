92862d49ed252dbb736c5935b44f223a2c0316f0
KIKO <fjmo_1997@hotmail.com>
2019-12-05 10:43:03 +0000

    updated install.sh and requirements

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ python_cmd="python3"
 
 $python_cmd -m venv env
 source ./env/bin/activate
-pip install --user -r requirements.txt
+pip install -r requirements.txt
 $python_cmd -m deeppavlov install ner_ontonotes_bert_mult
