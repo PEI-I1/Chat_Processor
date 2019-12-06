f022eb48a3e432501ef2aa6acf1e21bb42e7d323
jcm300 <zeclmartins@gmail.com>
2019-12-06 19:45:31 +0000

    Minor fix for docker port bind error

--- chat_processor/app.py
+++ chat_processor/app.py
@@ -15,4 +15,4 @@ def index():
     return get_response(req['idChat'], req['idUser'], req['msg'], req['name'])
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(host='0.0.0.0', port=5001)
