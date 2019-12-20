4d6cd12bf636e3de2bbcb80326fb268336865e6f
Fábio Gonçalves <a78793@alunos.uminho.pt>
2019-12-20 09:45:30 +0000

    Added more rules modes

--- chat_processor/rules_mode.py
+++ chat_processor/rules_mode.py
@@ -74,6 +74,28 @@ def remove_string(idChat, idUser, code):
     globals.redis_db.delete(str(idChat) + str(idUser) + code)
 
 
+def final_movies_options(idChat, idUser):
+    #_movie_genre
+    #_movie_cast
+    #_movie_producer
+    #_movie_synopsis
+    #_movie_age
+    #TODO
+
+def final_movies_duration(idChat, idUser):
+    #_search_term
+    #loc
+    #TODO
+
+def final_movies_loc(idChat, idUser):
+    #TODO
+
+def final_movies_semloc(idChat, idUser):
+    #TODO
+
+def final_movies_sessoes(idChat, idUser):
+    #TODO
+
 def final_packages(idChat, idUser):
     aux =  {}
     tipo = load_string(idChat, idUser, '_package_type')
@@ -176,48 +198,343 @@ def cinema_rules(idChat, idUser, menu, msg, chatData):
 
     if menu == 1:
         save_redis(idChat, idUser, 11)
-        return str('''Escolha uma das seguintes opções, digitando o número correspondente.
-1. procurar cinemas\n2. procurar filmes em sessão\n3. procurar filmes por parametro
-4. buscar próximas estreias\n5. buscar informação de um filme\n6. procurar sessões sobre uma duração específica
-7. procurar pelas próximas sessões\n8. procurar sessões de um filme\n9. procurar sessões por datas\n10. sair''')
+        return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. procurar cinemas\n2. procurar filmes em sessão\n3. procurar filmes por parametro\n4. buscar próximas estreias\n5. buscar informação de um filme\n6. procurar sessões sobre uma duração específica\n7. procurar pelas próximas sessões\n8. procurar sessões de um filme\n9. procurar sessões por data\n10. sair''')
 
     elif menu == 11:
         if opcao == 1:
-            #TODO Search for cinemas or get the closest ones
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 2:
-            #TODO Search for movies in cinema
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 3:
-            #TODO Search for movies based on genre, producer, cast, synopsis and age restriction
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 4:
-            #TODO Search for upcoming movies
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 5:
-            #TODO Get details of movie
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 6:
-            #TODO Search for sessions of movies under a certain duration
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 7:
-            #TODO Search for the next sessions
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 8:
-            #TODO Search sessions for a given movie
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 9:
-            #TODO Search for sessions by date
-            remove_redis(idChat, idUser, chatData)
-        elif opcao == 10:
-            remove_redis(idChat, idUser, chatData)
-            return str("Saiu do modo de regras.")
-        else:
-            return str('''Escolha uma das seguintes opções, digitando o número correspondente.
-1. procurar cinemas\n2. procurar filmes em sessão\n3. procurar filmes por parametro
-4. buscar próximas estreias\n5. buscar informação de um filme\n6. procurar sessões sobre uma duração específica
-7. procurar pelas próximas sessões\n8. procurar sessões de um filme\n9. procurar sessões por datas\n10. sair''')
+            save_redis(idChat, idUser, 12)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. procurar todos os cinemas\n2. procurar por um cinema\n3. procurar cinemas perto de localização\n4. sair\n''')
 
+        elif opcao == 2:
+            save_redis(idChat, idUser, 13)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. procurar filmes em todos os cinemas\n2. procurar filmes num cinema específico\n3. procurar filmes em cinemas perto de localização\n4. sair\n''')
+
+        elif opcao == 3:
+            save_redis(idChat, idUser, 14)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar o genero do filme\n2. espescificar cast\n3. especeficar produtor\n4. procurar sinopses\n5. especificar faixa etária\n4. sair\n''')
+
+        elif opcao == 4: # Apresentar próximas estreias
+            requerido = get_content('/scrapper/movies/releases', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+        elif opcao == 5:
+            save_redis(idChat, idUser, 15)
+            return str('''Especifique o filme que pretende obter infomações sobre.''')
+
+        elif opcao == 6:
+            save_redis(idChat, idUser, 16)
+            return str('''Especifique a duração do filme que pretende, em minutos.''')
+
+        elif opcao == 7:
+            save_redis(idChat, idUser, 17)
+            return str('''Pretende especificar algum parametro?\n1. sim\n2. não\n''')
+
+        elif opcao == 8:
+            save_redis(idChat, idUser, 18)
+            return str('''Especifique o nome do filme.''')
+
+        elif opcao == 9:
+            save_redis(idChat, idUser, 19)
+            return str('''Pretende especificar algum parametro?\n1. sim\n. não\n''')
+
+    elif menu == 19:
+        if opcao == 1:
+            save_redis(idChat, idUser, 191)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar termo de procura de cinema\n2. espescificar localização\n3. especeficar data (ano-mês-dia)\n4. especificar limite para início das sessões\n5. especificar limite para fim das sessões\n4. sair\n''')
+
+        if opcao == 2: # procurar sessoes por filme
+            requerido = get_content('/scrapper/sessions/by_date', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+    elif menu == 191:
+        if opcao == 1:
+            save_redis(idChat, idUser, 199)
+            return str('''Escreva termo de pesquisa para um cinema''')
+
+        if opcao == 2: # procurar sessoes por filme
+            save_redis(idChat, idUser, 198)
+            return str('''Especifique a sua localização''')
+
+        if opcao == 3: # procurar sessoes por filme
+            save_redis(idChat, idUser, 197)
+            return str('''Indique a data (ano-mês-dia)''')
+
+        if opcao == 4: # procurar sessoes por filme
+            save_redis(idChat, idUser, 196)
+            return str('''Especifique limite para início das sessões''')
+
+        if opcao == 5: # procurar sessoes por filme
+            save_redis(idChat, idUser, 195)
+            return str('''Especifique limite para fim das sessões''')
+
+    elif menu == 199:
+        save_string(idChat, idUser, '_search_term', msg)
+        save_redis(idChat, idUser, 19)
+        return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 198:
+        save_string(idChat, idUser, '_location', msg)
+        save_redis(idChat, idUser, 19)
+        return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 197:
+        save_string(idChat, idUser, '_date', msg)
+        save_redis(idChat, idUser, 19)
+        return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 196:
+        save_string(idChat, idUser, '_start_time', msg)
+        save_redis(idChat, idUser, 19)
+        return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 195:
+        save_string(idChat, idUser, '_end_time', msg)
+        save_redis(idChat, idUser, 19)
+        return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 18:
+        save_string(idChat, idUser, '_movie_name', msg)
+        save_redis(idChat, idUser, 180)
+            return str('''Pretende especificar algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 180:
+        if opcao == 1:
+            save_redis(idChat, idUser, 181)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar termo de procura de cinema\n2. espescificar localização\n3. especeficar data (ano-mês-dia)\n4. especificar limite para início das sessões\n5. especificar limite para fim das sessões\n4. sair\n''')
+
+        if opcao == 2: # procurar sessoes por filme
+            requerido = final_movies_sessoes(idChat, idUser)
+            #requerido = get_content('/scrapper/sessions/by_movie', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+    elif menu == 181:
+        if opcao == 1:
+            save_redis(idChat, idUser, 189)
+            return str('''Escreva termo de pesquisa para um cinema''')
+
+        if opcao == 2: # procurar sessoes por filme
+            save_redis(idChat, idUser, 188)
+            return str('''Especifique a sua localização''')
+
+        if opcao == 3: # procurar sessoes por filme
+            save_redis(idChat, idUser, 187)
+            return str('''Indique a data (ano-mês-dia)''')
+
+        if opcao == 4: # procurar sessoes por filme
+            save_redis(idChat, idUser, 186)
+            return str('''Especifique limite para início das sessões''')
+
+        if opcao == 5: # procurar sessoes por filme
+            save_redis(idChat, idUser, 185)
+            return str('''Especifique limite para fim das sessões''')
+
+    elif menu == 189:
+        save_string(idChat, idUser, '_package_price', 'all')
+        save_redis(idChat, idUser, 180)
+        return str('''Pretende especificar mais algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 188:
+        aux = {}
+        aux['lat'] = float(chatData['locationParam']['lat'])
+        aux['lon'] = float(chatData['locationParam']['lon'])
+        #save lat e lon
+        save_redis(idChat, idUser, 180)
+        return str('''Pretende especificar mais algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 187:
+        save_string(idChat, idUser, '_data', msg)
+        save_redis(idChat, idUser, 180)
+        return str('''Pretende especificar mais algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 186:
+        save_string(idChat, idUser, '_inicio', msg)
+        save_redis(idChat, idUser, 180)
+        return str('''Pretende especificar mais algum parametro de busca?\n1. sim\n2. não\n''')
+
+    elif menu == 185:
+        save_string(idChat, idUser, '_fim', msg)
+        save_redis(idChat, idUser, 180)
+        return str('''Pretende especificar mais algum parametro de busca?\n1. sim\n2. não\n''')
+
+
+    elif menu == 17:
+        if opcao == 1: # perguntar por termos
+            save_redis(idChat, idUser, 108)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar termo de procura de um cienma específico\n2. especificar localização''')
+
+        elif opcao == 2: # fazer a pesquisa sem localização
+            requerido = final_movies_semloc(idChat, idUser)
+            #requerido = get_content('/scrapper/sessions/next_sessions', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+    elif menu == 108:
+        if opcao == 1:
+            save_redis(idChat, idUser, 169)
+            return str('''Especifique termo de procura por cinema''')
+        elif opcao == 1:
+            save_redis(idChat, idUser, 168)
+            return str('''Especifique localização''')
+
+    elif menu == 169:
+        save_string(idChat, idUser, '_search_term', msg)
+        save_redis(idChat, idUser, 17)
+        return str('''Pretende especificar algum parametro?\n1. sim\n2. não\n''')
+
+    elif menu == 168:
+        aux = {}
+        aux['lat'] = float(chatData['locationParam']['lat'])
+        aux['lon'] = float(chatData['locationParam']['lon'])
+        save_redis(idChat, idUser, 110)
+        return str('''Pretende especificar algum parametro?\n1. sim\n2. não\n''')
+
+    elif menu == 110:
+        if opcao == 1:
+            save_redis(idChat, idUser, 108)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar termo de procura de um cienma específico\n2. especificar localização''')
+
+        elif opcao == 2: # fazer a pesquisa com localização
+            requerido = final_movies_loc(idChat, idUser)
+            #requerido = get_content('/scrapper/sessions/next_sessions', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+
+    elif menu == 16:
+        save_string(idChat, idUser, '_movie_duration', msg)
+        save_redis(idChat, idUser, 107)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu == 107:
+        if opcao == 1:
+            save_redis(idChat, idUser, 108)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. escrever expressão para procurar num cinema específico\n2. especificar localização\n3. especificar data\n4. especificar hora de início das sessões\n5. especificar hora de fim das sessões\n6. sair''')
+
+        elif opcao == 2: #fazer pesquisa de duração de filme
+            #duracao = load_string(idChat, idUser, '_movie_duration')
+            requerido = final_movies_duration(idChat, idUser)
+            #requerido = get_content('/scrapper/sessions/by_duration', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+    elif menu == 15: # obter info do filme
+        aux = {}
+        aux['movie'] = msg
+        requerido = get_content('/scrapper/movies/details', [], aux)
+        remove_redis(idChat, idUser, chatData)
+        return requerido
+
+    elif menu == 14:
+        if opcao == 1:
+            save_redis(idChat, idUser, 179)
+            return str('''Especifique que tipo de género pretende.''')
+
+        elif opcao == 2:
+            save_redis(idChat, idUser, 178)
+            return str('''Especifique o cast, separando cada nome por vírgula.''')
+
+        elif opcao == 3:
+            save_redis(idChat, idUser, 177)
+            return str('''Especifique um produtor.''')
+
+        elif opcao == 4:
+            save_redis(idChat, idUser, 176)
+            return str('''Especifique expressões para procurar na sinopse''')
+
+        elif opcao == 5:
+            save_redis(idChat, idUser, 175)
+            return str('''Especifique faixa etária''')
+
+    elif menu == 179:
+        save_string(idChat, idUser, '_movie_genre', msg)
+        save_redis(idChat, idUser, 106)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu == 178:
+        save_string(idChat, idUser, '_movie_cast', msg)
+        save_redis(idChat, idUser, 106)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu == 177:
+        save_string(idChat, idUser, '_movie_producer', msg)
+        save_redis(idChat, idUser, 106)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu == 176:
+        save_string(idChat, idUser, '_movie_synopsis', msg)
+        save_redis(idChat, idUser, 106)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu == 175:
+        save_string(idChat, idUser, '_movie_age', msg)
+        save_redis(idChat, idUser, 106)
+        return str('''Pretende especificar mais agum parametro na pesquisa?\n1. sim\n2. não\n''')
+
+    elif menu = 106:
+        if opcao == 1: # apresentar opções outra vez
+            save_redis(idChat, idUser, 14)
+            return str('''Escolha uma das seguintes opções, digitando o número correspondente.\n1. especificar o genero do filme\n2. espescificar cast\n3. especeficar produtor\n4. procurar sinopses\n5. especificar faixa etária\n4. sair\n''')
+
+        elif opcao == 2: # fazer a pesquisa
+            requerido = final_movies_options(idChat, idUser)
+            #requerido = get_content('/scrapper/movies/search', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+
+    elif menu == 13
+        if opcao == 1: #procurar cinemas sem dar nada
+            requerido = get_content('/scrapper/movies/by_cinema', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+            
+        elif opcao == 2: #procurar cinemas com query
+            save_redis(idChat, idUser, 102)
+            return str('''Insira uma expressão para procurar o cinema''')
+
+        elif opcao == 3: #procurar cinemas com lat e long
+            aux = {}
+            aux['lat'] = float(chatData['locationParam']['lat'])
+            aux['lon'] = float(chatData['locationParam']['lon'])
+            requerido = get_content('/scrapper/movies/by_cinema', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+
+    elif menu == 12:
+        if opcao == 1: #procurar cinemas sem dar nada
+            requerido = get_content('/scrapper/cinemas/search', [], {})
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+            
+        elif opcao == 2: #procurar cinemas com query
+            save_redis(idChat, idUser, 101)
+            return str('''Insira uma expressão para procurar o cinema''')
+
+        elif opcao == 3: #procurar cinemas com lat e long
+            aux = {}
+            aux['lat'] = float(chatData['locationParam']['lat'])
+            aux['lon'] = float(chatData['locationParam']['lon'])
+            requerido = get_content('/scrapper/cinemas/search', [], aux)
+            remove_redis(idChat, idUser, chatData)
+            return requerido
+
+    elif menu == 101: #procurar cinemas com query
+        aux = {}
+        aux['search_term'] = msg
+        requerido = get_content('/scrapper/cinemas/search', [], {})
+        remove_redis(idChat, idUser, chatData)
+        return requerido
+
+    elif menu == 102: #procurar cinemas com query
+        aux = {}
+        aux['search_term'] = msg
+        requerido = get_content('/scrapper/movies/by_cinema', [], aux)
+        remove_redis(idChat, idUser, chatData)
+        return requerido
 
 def fs_rules(idChat, idUser, menu, msg, chatData):
     try:
