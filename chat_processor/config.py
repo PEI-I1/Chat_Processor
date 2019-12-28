e486d242689382f3fe4cb57fb6eb0f3ed9599d5b
jcm300 <zeclmartins@gmail.com>
2019-12-28 22:19:06 +0000

    Improvement for deployment: config variables can be set by environment variables

--- chat_processor/config.py
+++ chat_processor/config.py
@@ -1,7 +1,9 @@
+import os
+
 urls = {
-    'API_ENDPOINT': "http://127.0.0.1:5000",
-    'FS_SCRAPER': 'http://127.0.0.1:5002',
-    'CINEMA_SCRAPER': 'http://127.0.0.1:5003',
-    'RS': 'http://127.0.0.1:5004',
-    'REDIS': {'host': 'localhost', 'port': 6379}
+    'API_ENDPOINT': os.getenv('API_ENDPOINT', "http://127.0.0.1:5000"),
+    'FS_SCRAPER': os.getenv('FS_SCRAPER', 'http://127.0.0.1:5002'),
+    'CINEMA_SCRAPER': os.getenv('CINEMA_SCRAPER', 'http://127.0.0.1:5003'),
+    'RS': os.getenv('RS', 'http://127.0.0.1:5004'),
+    'REDIS': {'host': os.getenv('REDIS_HOST', 'localhost'), 'port': os.getenv('REDIS_PORT', 6379)}
 }
