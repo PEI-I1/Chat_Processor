9407b380cebd95c75714a0238c15598bcd93ae62
MQuaresma <miguelquaresma97@gmail.com>
2020-01-14 19:21:20 +0000

    Fix route registering order

--- chat_processor/app.py
+++ chat_processor/app.py
@@ -3,17 +3,14 @@ import json
 from chat_processor import get_response, init as initCP
 from spell_checker import init as initSC
 
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    #init necessary components Chat Processor and Spell Checker
-    initCP()
-    initSC()
-    app.run(host='0.0.0.0', port=5001, threaded=True)
-
+app = Flask(__name__)
+#init necessary components Chat Processor and Spell Checker
+initCP()
+initSC()
 
 @app.route('/getResponse', methods=['POST'])
 def index():
+    print("HERE")
     req = request.get_json()
     req['msg'] = req['msg'].encode('utf-8').decode('utf-8')
     print(req)
@@ -25,3 +22,5 @@ def index():
     get_response(req['idChat'], req['idUser'], req['msg'], req['name'], location)
     return "success"
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, threaded=True)
